c0bebc920ca94128ac46673505250c4f1514bdbe
agni-c <agnibha0016@gmail.com>
2024-05-03 10:15:09 +0530

    Update page title and app name

--- configs/themeConfig.js
+++ configs/themeConfig.js
@@ -1,26 +1,26 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid'
 const themeConfig = {
   app: {
-    name: "Dashcode React",
+    name: 'E-KYC'
   },
-  // layout
+	// layout
   layout: {
     isRTL: false,
     darkMode: false,
     semiDarkMode: false,
-    skin: "default",
-    contentWidth: "full",
-    type: "vertical",
-    navBarType: "sticky",
-    footerType: "static",
+    skin: 'default',
+    contentWidth: 'full',
+    type: 'vertical',
+    navBarType: 'sticky',
+    footerType: 'static',
     isMonochrome: false,
     menu: {
       isCollapsed: false,
-      isHidden: false,
+      isHidden: false
     },
     mobileMenu: false,
-    customizer: false,
-  },
-};
+    customizer: false
+  }
+}
 
-export default themeConfig;
+export default themeConfig
